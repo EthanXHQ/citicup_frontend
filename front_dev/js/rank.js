a04d84d2e70a7fcc680d8d1128fc6278728bbdf8
cgj <2936127220@qq.com>
2022-03-17 17:56:51 +0800

    add the template of table

--- front_dev/js/rank.js
+++ front_dev/js/rank.js
@@ -6,3 +6,20 @@ j1.html('<span style = "color:green">JavaScript</span>');
 j2.text('JavaScript & ECMAScript');
 j1.html('<span style = "color:green">JavaScript</span>')
 
+function creatTable(data){
+    var tableData="<tr>"
+
+    for(var i=0;i<data.length;i++){
+      tableData+="<td>"+data[i]+"</td>"
+    }
+  
+    tableData+="</tr>"
+    var my_table=$(".default table")
+    my_table.html(my_table.html()+tableData)
+  }
+var arry = [1,2,3,4]
+for(var i=0;i<10;++i)
+{
+    creatTable(arry)
+}
+
