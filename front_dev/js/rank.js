70c7a604ae7eff45c2246b275ddb1e5f8d89b55a
cgj <2936127220@qq.com>
2022-03-20 01:23:39 +0800

    add the list os e,s,g and add the indo page ,css is not finished

--- front_dev/js/rank.js
+++ front_dev/js/rank.js
@@ -1,9 +1,25 @@
 var index1=0
+var index2=0
+var index3=0
+var index4=0
 function creatTable(data,target,index){
     var tableData="<tr>"
-
+    var implemented_code=""
+    for(var j=0;j<6-data[2].length;++j)
+    implemented_code+="0"
+    implemented_code+=data[2]
     for(var i=0;i<data.length;i++){
-      tableData+="<td>"+data[i]+"</td>"
+        if(i==1)
+        {
+            tableData+="<td><a id=\""+implemented_code+"\" href=\"javascript:void(0);\">"+data[1]+"</a></td>"
+            continue
+        }
+        else if (i==2)
+        {
+            tableData+="<td>"+implemented_code+"</td>"
+            continue
+        }
+        tableData+="<td>"+data[i]+"</td>"
     }
     
     tableData+="</tr>"
@@ -42,14 +58,41 @@ $.ajax(
         type: "GET",
         dataType: "JSON",
         success:function(data){
+            //console.log(data)
             ls=data
-            var cur=0
-            for (var i =cur ;i < cur+10;++i)
+            //TO DO:  here to finish the rank
+            ls1=ls
+            ls2=ls
+            ls3=ls
+            ls4=ls
+            //here
+            for (var i =0 ;i < 10;++i)
             {
-                arry=[i+1,data[i]["Stock_code"],data[i]["Com_name"],data[i]["Industry"],data[i]["ESG_Rating"]]
+                arry=[index1+1,ls1[i]["Com_name"],ls1[i]["Stock_code"],ls1[i]["Industry"],ls1[i]["ESG_Rating"]]
                 creatTable(arry,".default table",index1)
                 ++index1
             }
+            for (var i =0 ;i < 10;++i)
+            {
+                arry=[index2+1,ls2[i]["Com_name"],ls2[i]["Stock_code"],ls2[i]["Industry"],ls2[i]["Environmental_Rating"]]
+                creatTable(arry,".E table",index2)
+                ++index2
+            }
+            for (var i =0 ;i < 10;++i)
+            {
+                arry=[index3+1,ls3[i]["Com_name"],ls3[i]["Stock_code"],ls3[i]["Industry"],ls3[i]["Social_Rating"]]
+                creatTable(arry,".S table",index3)
+                ++index3
+            }
+            for (var i =0 ;i < 10;++i)
+            {
+                arry=[index4+1,ls4[i]["Com_name"],ls4[i]["Stock_code"],ls4[i]["Industry"],ls4[i]["Governance_Rating"]]
+                creatTable(arry,".G table",index4)
+                ++index4
+            }
+            $('a').click(function(){
+                Jump2Cm_info($(this).attr("id"))
+            })
         },
         error:function(res,StatusText)
         {
@@ -68,14 +111,81 @@ $(".bt1").click(function(){
     var cur=index1
     for (var i =cur ;i < cur+10;++i)
     {
-        arry=[index1+1,ls[i]["Stock_code"],ls[i]["Com_name"],ls[i]["Industry"],ls[i]["ESG_Rating"]]
+        arry=[index1+1,ls1[i]["Com_name"],ls1[i]["Stock_code"],ls1[i]["Industry"],ls1[i]["ESG_Rating"]]
         creatTable(arry,".default table",index1)
         ++index1
     }
-    //window.open("../html/cminfo.html")
+    $('a').click(function(){
+        Jump2Cm_info($(this).attr("id"))
+    })
+})
+
+$(".bt2").click(function(){
+    var cur=index2
+    for (var i =cur ;i < cur+10;++i)
+    {
+        arry=[index2+1,ls2[i]["Com_name"],ls2[i]["Stock_code"],ls2[i]["Industry"],ls2[i]["Environmental_Rating"]]
+        creatTable(arry,".E table",index2)
+        ++index2
+    }
+    $('a').click(function(){
+        Jump2Cm_info($(this).attr("id"))
+    })
+})
+
+$(".bt3").click(function(){
+    var cur=index3
+    for (var i =cur ;i < cur+10;++i)
+    {
+        arry=[index3+1,ls3[i]["Com_name"],ls3[i]["Stock_code"],ls3[i]["Industry"],ls3[i]["Social_Rating"]]
+        creatTable(arry,".S table",index3)
+        ++index3
+    }
+    $('a').click(function(){
+        Jump2Cm_info($(this).attr("id"))
+    })
+})
+
+$(".bt4").click(function(){
+    var cur=index4
+    for (var i =cur ;i < cur+10;++i)
+    {
+        arry=[index4+1,ls4[i]["Com_name"],ls4[i]["Stock_code"],ls4[i]["Industry"],ls4[i]["Governance_Rating"]]
+        creatTable(arry,".G table",index4)
+        ++index4
+    }
+    $('a').click(function(){
+        Jump2Cm_info($(this).attr("id"))
+    })
 })
 
 $(".bt-1").click(function(){
     $(".default table").html('<tr class="default head"><th>排名</th><th>公司名称</th><th>证券代码</th><th>产业类别</th><th>ESG综合得分</th></tr>')
     index1=0
 })
+$(".bt-2").click(function(){
+    $(".E table").html('<tr class="default head"><th>排名</th><th>公司名称</th><th>证券代码</th><th>产业类别</th><th>E 得分</th></tr>')
+    index2=0
+})
+$(".bt-3").click(function(){
+    $(".S table").html('<tr class="default head"><th>排名</th><th>公司名称</th><th>证券代码</th><th>产业类别</th><th>S 得分</th></tr>')
+    index3=0
+})
+$(".bt-4").click(function(){
+    $(".G table").html('<tr class="default head"><th>排名</th><th>公司名称</th><th>证券代码</th><th>产业类别</th><th>G 得分</th></tr>')
+    index4=0
+})
+//here to realize the jump
+//first , all the com_name is bounded to a link
+
+//console.log($("a"))
+
+function Jump2Cm_info(id)
+{
+    console.log(id)
+    let params = {
+        "id": id,
+    };
+    window["filter"] = params;
+    window.open("../html/cminfo.html#"+id);
+}
